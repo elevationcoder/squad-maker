603da2983e985aa8d866ec534e7e38802912c17d
cosmicChimp <ian.t.marrs@gmail.com>
2020-05-11 20:27:54 -0600

    added changes to schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,21 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_021502) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "characters", force: :cascade do |t|
-    t.string "name"
-    t.string "age"
-    t.string "weight"
-    t.string "height"
-    t.string "sex"
-    t.string "race"
-    t.string "klass"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
